c925501abe3ab992ab61bf37dadd97c0dd15f0cb
Arnoldo Marin Ahijado <37191387+arnoldomarin@users.noreply.github.com>
2024-02-06 16:07:05 +0100

    added logic to show mobile view nav component when device is mobile/tablet

--- src/components/Resizable/ResizableComponent.tsx
+++ src/components/Resizable/ResizableComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import {
     ResizableHandle,
@@ -12,6 +12,16 @@ import VerticalNavbar from '../Navs/VerticalNavbar'
 import { CarFront, Receipt, BadgeDollarSign, GanttChartSquare, MenuSquare, HelpCircle, UserPlus, BookText, ShieldEllipsis } from 'lucide-react';
 import { Toggle } from "@/components/ui/toggle";
 import Navbar from '../Navbar';
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
 
 const ResizableComponent = () => {
 
@@ -75,21 +85,55 @@ const ResizableComponent = () => {
         setNavbarCollapsed(event);
     };
 
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 768); // Adjust the width as needed
+      };
+  
+      // Initial check on component mount
+      handleResize();
+  
+      // Listen to window resize events
+      window.addEventListener('resize', handleResize);
+  
+      // Cleanup event listener on component unmount
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
     return (
         <div>
-
-            <ResizablePanelGroup direction="horizontal" className='flex flex-col align-middle min-h-screen'>     
-                <ResizablePanel minSize={navbarCollapsed ? 3.5 : 12} maxSize={navbarCollapsed ? 3.5 : 15} className='border'>
-                <div className="p-2 border-b">
-                    <Toggle onPressedChange={(e) => handleChange(e)}><MenuSquare size={20} /></Toggle>
-                </div>  
-                    <VerticalNavbar isCollapsed={navbarCollapsed} links={dfBusinessCategs} supportLinks={SupportLinks}/>
-                </ResizablePanel>
-            <ResizableHandle withHandle={!navbarCollapsed} className='bg-transparent w-0'/>
-                <ResizablePanel>   
+            {isMobile ? (
+                // Mobile view component
+                <Sheet>
+                <SheetTrigger>Open</SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                    <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+                </Sheet>
+            ) : (
+                <ResizablePanelGroup direction="horizontal" className='flex flex-col align-middle min-h-screen'>     
+                    <ResizablePanel minSize={navbarCollapsed ? 3.5 : 12} maxSize={navbarCollapsed ? 3.5 : 15} className='border smooth-transition'>
+                        <div className="p-2 border-b">
+                            <Toggle onPressedChange={(e) => handleChange(e)}><MenuSquare size={20} /></Toggle>
+                        </div>  
+                        <VerticalNavbar isCollapsed={navbarCollapsed} links={dfBusinessCategs} supportLinks={SupportLinks}/>
+                    </ResizablePanel>
+                <ResizableHandle withHandle={!navbarCollapsed} className='bg-transparent w-0'/>
+                    <ResizablePanel>   
                         <Navbar />
-                </ResizablePanel>
-            </ResizablePanelGroup>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            )}
         </div>
     )
 }
