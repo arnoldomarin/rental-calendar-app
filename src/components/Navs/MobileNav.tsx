6ee0db5f47b6d4e3af0892cd2c6163c0348db2b3
Arnoldo Marin Ahijado <37191387+arnoldomarin@users.noreply.github.com>
2024-02-07 10:01:11 +0100

    fixed all the styling for accordions in mobile view menu

--- src/components/Navs/MobileNav.tsx
+++ src/components/Navs/MobileNav.tsx
@@ -62,27 +62,27 @@ const MobileNav = () => {
             <SheetDescription>
             <Accordion type="multiple" className="w-full">
               <AccordionItem value="item-1">
-                <AccordionTrigger>Assets</AccordionTrigger>
+                <AccordionTrigger className="text-black">Assets</AccordionTrigger>
                 <AccordionContent>
                   {assetsGeneralLinks.map((component) => (
                     <Link
                       key={component.title}
                       href={component.href}
-                      className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                      className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                     >
                       <span>{component.title}</span>
                     </Link>
                   ))}
                   {/* Nested Accordion */}
-                  <Accordion type="multiple">
+                  <Accordion type="multiple" className='sub-accordions'>
                     <AccordionItem value="nested-item-1">
-                      <AccordionTrigger>Assets Inventory</AccordionTrigger>
+                      <AccordionTrigger className="text-black text-xs">Assets Inventory</AccordionTrigger>
                       <AccordionContent>
                         {assetsInventoryLinks.map((component) => (
                           <Link
                             key={component.title}
                             href={component.href}
-                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                           >
                             <span>{component.title}</span>
                           </Link>
@@ -90,13 +90,13 @@ const MobileNav = () => {
                       </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="nested-item-2">
-                      <AccordionTrigger>Assets Transfer</AccordionTrigger>
+                      <AccordionTrigger className="text-black text-xs">Assets Transfer</AccordionTrigger>
                       <AccordionContent>
                         {assetsTransferLinks.map((component) => (
                           <Link
                             key={component.title}
                             href={component.href}
-                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                           >
                             <span>{component.title}</span>
                           </Link>
@@ -107,27 +107,27 @@ const MobileNav = () => {
                 </AccordionContent>
               </AccordionItem>
               <AccordionItem value="item-2">
-                <AccordionTrigger>Rental</AccordionTrigger>
+                <AccordionTrigger className="text-black">Rental</AccordionTrigger>
                 <AccordionContent>
                   {rentalGeneralLinks.map((component) => (
                     <Link
                       key={component.title}
                       href={component.href}
-                      className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                      className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                     >
                       <span>{component.title}</span>
                     </Link>
                   ))}
                   {/* Nested Accordion */}
-                  <Accordion type="multiple">
+                  <Accordion type="multiple" className='sub-accordions'>
                     <AccordionItem value="nested-item-1">
-                      <AccordionTrigger>Rental Utilization</AccordionTrigger>
+                      <AccordionTrigger className="text-black text-xs">Rental Utilization</AccordionTrigger>
                       <AccordionContent>
                         {rentalUtilizationLinks.map((component) => (
                           <Link
                             key={component.title}
                             href={component.href}
-                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                           >
                             <span>{component.title}</span>
                           </Link>
@@ -135,13 +135,13 @@ const MobileNav = () => {
                       </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="nested-item-2">
-                      <AccordionTrigger>Rental Invoices</AccordionTrigger>
+                      <AccordionTrigger className="text-black text-xs">Rental Invoices</AccordionTrigger>
                       <AccordionContent>
                         {rentalInvoicesLinks.map((component) => (
                           <Link
                             key={component.title}
                             href={component.href}
-                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                           >
                             <span>{component.title}</span>
                           </Link>
@@ -149,13 +149,13 @@ const MobileNav = () => {
                       </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="nested-item-3">
-                      <AccordionTrigger>Rental Revenue</AccordionTrigger>
+                      <AccordionTrigger className="text-black text-xs">Rental Revenue</AccordionTrigger>
                       <AccordionContent>
                         {rentalRevenueLinks.map((component) => (
                           <Link
                             key={component.title}
                             href={component.href}
-                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                            className="flex flex-wrap justify-startdark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white text-xs my-2"
                           >
                             <span>{component.title}</span>
                           </Link>
